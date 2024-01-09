f04e89bdc7aecbcd398af3a165d7cd67f6a73581
Lucifer3025 <156141595+Lucifer3025@users.noreply.github.com>
2024-01-10 04:27:09 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+.profile-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+}
+
+.profile-photo {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-right: 10px;
+}
